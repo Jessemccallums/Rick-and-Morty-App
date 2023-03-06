f9aab451a5816c2325f3e4fb9b144d35e39ca4fb
JesseMcCallums <jessemccallums2001@gmail.com>
2023-03-06 18:04:45 -0400

    Update file

---
+++ src/components/Cards.jsx
@@ -0,0 +1,14 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   const { characters } = props;
+   return(
+      
+     <div>
+      {characters.map((el) => {
+         return <Card name={el.name} species={el.species}
+         gender={el.gender} image={el.image} onClose={() => window.alert('Emulamos que se cierra la card')}/>
+      })}
+   </div>
+   )
+}
